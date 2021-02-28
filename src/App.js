23ccae1f2999c82e90684ad50399f2aed9fb09ad
Deivid <david_recheni@hotmail.com>
2021-02-28 22:59:04 +0100

    Rotacion automatica

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import React, { Suspense } from 'react'
+import React, { Suspense, useState } from 'react'
 import { Canvas } from 'react-three-fiber'
 
 import Model from './Model/Model'
@@ -9,21 +9,27 @@ import './App.css'
 
 function App() {
 
+  const [rx, setRx] = useState(0)
+  const [ry, setRy] = useState(0)
+  const [rz, setRz] = useState(0)
+
+
+  
   return (
-      <Canvas
-        camera={{ position: [0, 0, -10], fov: 50 }}
-        onCreated={({ camera, gl, scene }) => {
-          camera.lookAt(new THREE.Vector3(0, 0, 0))
-          scene.background = new THREE.Color('lightblue')
-          gl.shadowMap.enabled = true
-          gl.shadowMap.type = THREE.PCFSoftShadowMap
-        }}>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Suspense fallback={<Box color="orange" scale={[3, 3, 3]}></Box>}>
-          <Model />
-        </Suspense>
-      </Canvas>
+    <Canvas
+      camera={{ position: [0, 0, -10], fov: 50 }}
+      onCreated={({ camera, gl, scene }) => {
+        camera.lookAt(new THREE.Vector3(0, 0, 0))
+        scene.background = new THREE.Color('lightblue')
+        gl.shadowMap.enabled = true
+        gl.shadowMap.type = THREE.PCFSoftShadowMap
+      }}>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Suspense fallback={<Box color="orange" scale={[3, 3, 3]}></Box>}>
+        <Model rotation={[rx, ry, rz]} />
+      </Suspense>
+    </Canvas>
   );
 }
 
--- src/Model/Model.js
+++ src/Model/Model.js
@@ -3,11 +3,14 @@ import { useFrame, useLoader } from 'react-three-fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import ShapeGLTF from '../Assets/model.glb'
 
-export default function Model() {
+export default function Model({ rotation }) {
     const mixer = useRef()
-    
+
     const gltf = useLoader(GLTFLoader, ShapeGLTF)
 
-    useFrame(({ clock }) => mixer.current && mixer.current.update(clock.getDelta()))
-    return gltf ? <primitive object={gltf.scene} color='orange' scale={[.02, .02, .02]} /> : null
+    useFrame(() => mixer.current.rotation.y += 0.01)
+
+
+    // TODO Link rotation angle to mouse.
+    return gltf ? <primitive ref={mixer} object={gltf.scene} color='orange' position={[-2.5, 0, 0]} rotation={rotation} scale={[1, 1, 1]} /> : null
 }
